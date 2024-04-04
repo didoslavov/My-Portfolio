9cc6fea8e0ccb3e4c651ae81eac08934ab692a33
didoslavov <didkoslawow@gmail.com>
2024-04-05 02:00:53 +0300

    add navigation component

---
+++ app/Header/Nav.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav className={`w-full lg:flex lg:justify-end hidden md:flex md:justify-end`}>
+            <ul className="flex lg:justify-end text-md uppercase justify-around gap-4 text-oxford-blue dark:text-timberwolf">
+                <li>
+                    <a href="#">Projects</a>
+                </li>
+                <li>
+                    <a href="#">About me</a>
+                </li>
+                <li>
+                    <a href="#">Contacts</a>
+                </li>
+                <li>
+                    <a href="#">Guest book</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
