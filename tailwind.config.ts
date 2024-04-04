1309c6e819eb9cd51a8dd134557e6c3099361062
didoslavov <didkoslawow@gmail.com>
2024-04-05 01:59:44 +0300

    add color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,17 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
     content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+    darkMode: 'class',
     theme: {
         extend: {},
+        colors: {
+            timberwolf: '#CDCDC3',
+            'sheen-gold': '#CAA45D',
+            'harvest-gold': '#CC922F',
+            'prussian-blue': '#1E3957',
+            'oxford-blue': '#0A2749',
+            'rich-black': '#0F1F2F',
+        },
     },
     plugins: [],
 };
