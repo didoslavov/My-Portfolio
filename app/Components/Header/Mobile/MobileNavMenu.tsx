146aef6d3ca429323878ee43082c686b61ac2430
didoslavov <didkoslawow@gmail.com>
2024-04-05 15:43:59 +0300

    add animation to mobile menu

--- app/Components/Header/Mobile/MobileNavMenu.tsx
+++ app/Components/Header/Mobile/MobileNavMenu.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import MobileButton from './MobileButton';
 
-const MobileNavMenu = () => {
+gsap.registerPlugin(useGSAP);
+
+let listItemTween: gsap.core.Tween;
+
+const MobileNavMenu = ({ isMobileMenuOpen, toggleMobile }: { isMobileMenuOpen: boolean; toggleMobile: () => void }) => {
+    useGSAP(() => {
+        gsap.from('.menu', { opacity: 0 });
+        gsap.to('.menu', { opacity: 1, height: '100vh', ease: 'expo.inOut' });
+
+        listItemTween = gsap.from('.li', { opacity: 0, y: -200, stagger: 0.2, ease: 'expo.inOut' });
+    });
+
+    const toggleMobileMenu = () => {
+        listItemTween.reverse().then(() => {
+            toggleMobile();
+        });
+    };
+
     return (
-        <div className="text-oxford-blue dark:text-timberwolf flex items-center justify-center backdrop-blur-2xl py-4 px-4 absolute top-0 -right-12 w-screen h-screen shadow-md z-40 lg:hidden md:hidden">
-            <ul className="text-4xl">
-                <li className="mb-8">
-                    <a href="#">Projects</a>
-                </li>
-                <li className="mb-8">
-                    <a href="#">About me</a>
-                </li>
-                <li className="mb-8">
-                    <a href="#">Contacts</a>
-                </li>
-                <li className="mb-8">
-                    <a href="#">Guest book</a>
-                </li>
-            </ul>
-        </div>
+        <>
+            {isMobileMenuOpen && <MobileButton isMobileMenuOpen={isMobileMenuOpen} toggleMobileMenu={toggleMobileMenu} />}
+            <div className="menu text-oxford-blue dark:text-timberwolf flex items-center justify-center backdrop-blur-2xl py-4 px-4 absolute top-0 -right-12 w-screen h-screen shadow-md z-40 lg:hidden md:hidden">
+                <ul className="text-4xl">
+                    <li className="mb-8 li">
+                        <a href="#">Projects</a>
+                    </li>
+                    <li className="mb-8 li">
+                        <a href="#">About me</a>
+                    </li>
+                    <li className="mb-8 li">
+                        <a href="#">Contacts</a>
+                    </li>
+                    <li className="mb-8 li">
+                        <a href="#">Guest book</a>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 };
 
