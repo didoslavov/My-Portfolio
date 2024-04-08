e41ce714d41e04854233f0d8f11d31887a46213f
didoslavov <didkoslawow@gmail.com>
2024-04-08 05:15:55 +0300

    add duration to animation and fix container width

--- app/Components/Header/Mobile/MobileNavMenu.tsx
+++ app/Components/Header/Mobile/MobileNavMenu.tsx
@@ -9,8 +9,8 @@ let listItemTween: gsap.core.Tween;
 const MobileNavMenu = ({ isMobileMenuOpen, toggleMobile }: { isMobileMenuOpen: boolean; toggleMobile: () => void }) => {
     useGSAP(
         () => {
-            gsap.from('.menu', { opacity: 0 });
-            gsap.to('.menu', { opacity: 1, height: '100vh', ease: 'power1.in' });
+            gsap.from('.menu', { duration: 1, opacity: 0 });
+            gsap.to('.menu', { duration: 1, opacity: 1, height: '100vh', ease: 'power1.in' });
 
             listItemTween = gsap.from('.li', { opacity: 0, y: -200, stagger: 0.2, ease: 'power1.out' });
         },
@@ -26,7 +26,7 @@ const MobileNavMenu = ({ isMobileMenuOpen, toggleMobile }: { isMobileMenuOpen: b
     return (
         <>
             {isMobileMenuOpen && <MobileButton isMobileMenuOpen={isMobileMenuOpen} toggleMobileMenu={toggleMobileMenu} />}
-            <div className="menu text-raisin-black dark:text-silver flex items-center justify-center backdrop-blur-md py-4 px-4 absolute top-0 -right-12 w-screen h-screen shadow-md z-40 lg:hidden md:hidden">
+            <div className="menu text-raisin-black dark:text-silver flex items-center justify-center backdrop-blur-md py-4 px-4 absolute top-0 right-0 w-full h-screen shadow-md z-40 lg:hidden md:hidden">
                 <ul className="text-4xl">
                     <li className="mb-8 li">
                         <a href="#">Projects</a>
