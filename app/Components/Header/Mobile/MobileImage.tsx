0e4e17144c81e13df4c19c654cfda4c76455937b
didoslavov <didkoslawow@gmail.com>
2024-04-08 05:15:28 +0300

    add animation to mobile img transition

--- app/Components/Header/Mobile/MobileImage.tsx
+++ app/Components/Header/Mobile/MobileImage.tsx
@@ -1,3 +1,5 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 import Image from 'next/image';
 import React from 'react';
 
@@ -6,6 +8,14 @@ interface MobileImageProps {
 }
 
 const MobileImage: React.FC<MobileImageProps> = ({ isMobileMenuOpen }) => {
+    useGSAP(
+        () => {
+            gsap.from('.mobile-img', { duration: 0.5, opacity: 0, ease: 'power1.in' });
+            gsap.to('.mobile-img', { duration: 0.5, opacity: 1, ease: 'power1.in' });
+        },
+        { dependencies: [isMobileMenuOpen] }
+    );
+
     return (
         <>
             <Image
@@ -13,7 +23,7 @@ const MobileImage: React.FC<MobileImageProps> = ({ isMobileMenuOpen }) => {
                 height={500}
                 src="/portfolio-img-bg-light.jpg"
                 alt="Picture of me, Dido"
-                className={`rounded-full w-14 h-14 drop-shadow-2xl  dark:hidden md:hidden lg:hidden ${
+                className={`mobile-img rounded-full w-14 h-14 drop-shadow-2xl  dark:hidden md:hidden lg:hidden ${
                     isMobileMenuOpen ? 'hidden' : 'block'
                 }`}
             />
@@ -22,7 +32,7 @@ const MobileImage: React.FC<MobileImageProps> = ({ isMobileMenuOpen }) => {
                 height={500}
                 src="/portfolio-img-bg-dark.jpg"
                 alt="Picture of me, Dido"
-                className={`rounded-full w-14 h-14 drop-shadow-2xl hidden dark:block md:dark:hidden lg:dark:hidden ${
+                className={`mobile-img rounded-full w-14 h-14 drop-shadow-2xl hidden dark:block md:dark:hidden lg:dark:hidden ${
                     isMobileMenuOpen ? 'hidden' : 'block'
                 }`}
             />
