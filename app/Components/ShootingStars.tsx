6321ee796dc61f8f904ea33b88d0018a91042a65
didoslavov <didkoslawow@gmail.com>
2024-04-10 05:04:18 +0300

    add shooting star bg animation

--- app/Components/ShootingStars.tsx
+++ app/Components/ShootingStars.tsx
@@ -1,73 +1,72 @@
 'use client';
-import { AiFillStar } from 'react-icons/ai';
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { AiFillStar } from 'react-icons/ai';
+
+const Star = () => {
+    gsap.ticker.lagSmoothing(false);
+
+    const leftPosition = `${Math.random() * 100}vw`;
+    const starRef = useRef<HTMLDivElement>(null);
+    const trailRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() => {
+        const duration = Math.random() * 1.5 + 3;
+        const starElement = starRef.current;
+        const trailElement = trailRef.current;
+
+        const tl = gsap.timeline({ repeat: -1 });
+
+        tl.to([starElement], {
+            opacity: 0,
+            x: '-50vw',
+            y: '100vh',
+            duration,
+            ease: 'sine',
+        });
+
+        tl.to(
+            trailElement,
+            {
+                opacity: 0,
+                height: 0,
+                duration,
+                ease: 'sine',
+            },
+            8
+        );
+    });
+
+    return (
+        <>
+            <div className="star absolute top-2" style={{ left: leftPosition }} ref={starRef}>
+                <div
+                    ref={trailRef}
+                    className="opacity-60 bg-wine dark:bg-silver w-[1px] h-16 rounded-full rotate-45 absolute -top-[56px] -right-[24px]"></div>
+                <AiFillStar size={6} className="opacity-60 text-raisin-black dark:text-silver" />
+            </div>
+        </>
+    );
+};
 
 const ShootingStars = () => {
-    useGSAP(() => {
-        const createStar = () => {
-            const mainStar = document.createElement('div');
-            mainStar.innerHTML = '&star;';
-            mainStar.classList.add('star', 'w-1', 'h-1', 'text-raisin-black', 'dark:text-silver', 'absolute', 'rounded-full');
-            mainStar.style.left = `${Math.random() * 100}vw`;
-            mainStar.style.top = '0';
-            document.getElementById('stars')?.appendChild(mainStar);
+    const [stars, setStars] = useState<Array<JSX.Element>>([]);
 
-            const numTrailStars = 5;
-            const trailStars: HTMLDivElement[] = [];
-
-            for (let i = 0; i < numTrailStars; i++) {
-                const trailStar = document.createElement('div');
-                trailStar.classList.add(
-                    'trail-star',
-                    'w-1',
-                    'h-1',
-                    'bg-raisin-black',
-                    'dark:bg-silver',
-                    'absolute',
-                    'rounded-full'
-                );
-                trailStar.style.left = mainStar.style.left;
-                trailStar.style.top = mainStar.style.top;
-                document.getElementById('stars')?.appendChild(trailStar);
-                trailStars.push(trailStar);
-            }
-
-            gsap.to(mainStar, {
-                opacity: 0,
-                x: '-100vw',
-                y: '100vh',
-                duration: Math.random() * 1.5 + 5,
-                ease: 'power1.inOut',
-                onComplete: () => {
-                    mainStar.remove();
-                    trailStars.forEach((trailStar) => trailStar.remove());
-                },
-            });
-
-            trailStars.forEach((trailStar, index) => {
-                gsap.to(trailStar, {
-                    opacity: 0,
-                    x: () => `${parseFloat(mainStar.style.left) - index * 10}px`,
-                    y: '+=20',
-                    duration: Math.random() * 1 + 1,
-                    delay: index * 0.1,
-                    ease: 'power1.inOut',
-                    onComplete: () => {
-                        trailStar.remove();
-                    },
-                });
-            });
-        };
-
-        const interval = setInterval(createStar, 1000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setStars((prevStars) => [...prevStars, <Star key={prevStars.length} />]);
+        }, 500);
 
         return () => clearInterval(interval);
     }, []);
 
-    return <div id="stars" className="absolute top-0 left-0 w-full h-full pointer-events-none z-0"></div>;
+    return (
+        <div id="stars" className="absolute top-0 left-0 w-full h-full pointer-events-none z-0">
+            {stars}
+        </div>
+    );
 };
 
 export default ShootingStars;
