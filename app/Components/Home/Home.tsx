524dce2e7e1a6af25fb47054a71927e85dc957ed
didoslavov <didkoslawow@gmail.com>
2024-04-12 03:46:26 +0300

    position social icons in the right place

--- app/Components/Home/Home.tsx
+++ app/Components/Home/Home.tsx
@@ -15,6 +15,7 @@ const Home = () => {
     const stack = ['Java Script', 'React', 'Next', 'Node JS', 'Express'];
     const containerRef = useRef<HTMLDivElement>(null);
     const contentRef = useRef<HTMLDivElement>(null);
+    const socialsRef = useRef<HTMLUListElement>(null);
 
     useGSAP(() => {
         const vsOpts = {
@@ -47,15 +48,11 @@ const Home = () => {
 
         vSlide.play();
 
-        const animateContent = () => {
-            gsap.from(contentRef.current, {
-                transform: 'translateY(-100%)',
-                duration: 2,
-                ease: 'power3.out',
-            });
-        };
-
-        animateContent();
+        gsap.from(contentRef.current, {
+            transform: 'translateY(-100%)',
+            duration: 2,
+            ease: 'power3.out',
+        });
     });
 
     const handleHover = () => {
@@ -63,14 +60,17 @@ const Home = () => {
             const expandingDivs = Array.from(containerRef.current.querySelectorAll('.expanding-div')) as HTMLElement[];
             expandingDivs.forEach((div, i) => {
                 gsap.to(div, {
-                    width: i === 1 ? '120%' : i === 2 ? '110%' : '120%',
+                    width: i === 1 ? '115%' : i === 2 ? '105%' : '125%',
                     scale: i === 1 ? 1.1 : i === 2 ? 1.05 : 1.15,
                     duration: 0.5,
-                    x: i === 1 ? '0%' : i === 2 ? '10%' : '10%',
-                    y: i === 1 ? '0%' : i === 2 ? '-10%' : '10%',
+                    x: i === 1 ? '20%' : i === 2 ? '-12%' : '-14%',
+                    y: i === 1 ? '0%' : i === 2 ? '-14%' : '14%',
+                    ease: 'expo.out',
                 });
             });
         }
+
+        gsap.to(socialsRef.current, { opacity: 1, duration: 1 });
     };
 
     const handleHoverExit = () => {
@@ -78,15 +78,18 @@ const Home = () => {
             const expandingDivs = Array.from(containerRef.current.querySelectorAll('.expanding-div')) as HTMLElement[];
             expandingDivs.forEach((div) => {
                 gsap.to(div, {
+                    duration: 0.5,
                     width: '100%',
                     scaleX: 1,
                     scaleY: 1,
                     x: '0%',
                     y: '0%',
-                    duration: 0.5,
+                    ease: 'expo.in',
                 });
             });
         }
+
+        gsap.to(socialsRef.current, { opacity: 0, duration: 1 });
     };
     return (
         <div
@@ -106,23 +109,6 @@ const Home = () => {
                     </div>
                     <p className="leading-[50px]">Developer</p>
                 </div>
-                <ul className="flex md:justify-center gap-6 text-raisin-black dark:text-silver">
-                    <li>
-                        <a href="#" className="text-wine dark:text-sheen-gold">
-                            <AiFillGithub size={30} />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-wine dark:text-sheen-gold">
-                            <AiFillLinkedin size={30} />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-wine dark:text-sheen-gold">
-                            <IoIosPaper size={30} />
-                        </a>
-                    </li>
-                </ul>
             </div>
             <div className="relative" ref={containerRef} onMouseEnter={handleHover} onMouseLeave={handleHoverExit}>
                 <Image
@@ -139,7 +125,27 @@ const Home = () => {
                     alt="Picture of me, Dido"
                     className="z-50 relative rounded-xl md:w-[360px] md:h-[470px] hidden md:hidden md:dark:block"
                 />
-                <div className="expanding-div z-10 rounded-xl md:w-[360px] md:h-[470px] hidden md:block opacity-50 absolute -top-6 -right-6 bg-wine-700 dark:bg-sheen-gold-100"></div>
+                <div className="expanding-div z-10 rounded-xl md:w-[360px] md:h-[470px] hidden md:block opacity-50 absolute -top-6 -right-6 bg-wine-700 dark:bg-sheen-gold-100">
+                    <ul
+                        ref={socialsRef}
+                        className="flex flex-col absolute top-4 left-4 md:justify-center gap-6 text-raisin-black dark:text-silver">
+                        <li>
+                            <a href="#" className="text-silver dark:text-raisin-black">
+                                <AiFillGithub size={30} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-silver dark:text-raisin-black">
+                                <AiFillLinkedin size={30} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-silver dark:text-raisin-black">
+                                <IoIosPaper size={30} />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
                 <div className="expanding-div z-10 rounded-xl md:w-[360px] md:h-[470px] hidden md:block opacity-50 absolute -top-4 -right-4 bg-wine-800 dark:bg-sheen-gold-200"></div>
                 <div className="expanding-div z-10 rounded-xl md:w-[360px] md:h-[470px] hidden md:block opacity-50 absolute -top-2 -right-2 bg-wine-900 dark:bg-sheen-gold-300"></div>
             </div>
