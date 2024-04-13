d58681c1bb091d1fb5b0f157a622ccbfc58045a0
didoslavov <didkoslawow@gmail.com>
2024-04-13 23:20:53 +0300

    playing around with canvas and pixijs

--- app/Components/ShootingStars/Canvas.tsx
+++ app/Components/ShootingStars/Canvas.tsx
@@ -6,49 +6,27 @@ import gsap from 'gsap';
 import { PixiPlugin } from 'gsap/PixiPlugin';
 import { useGSAP } from '@gsap/react';
 import { Application } from 'pixi.js';
+import { Sprite, Stage } from '@pixi/react';
 
 gsap.registerPlugin(PixiPlugin);
 PixiPlugin.registerPIXI(PIXI);
 
-const Canvas = () => {
-    const canvasRef = useRef<HTMLDivElement>(null);
+const Star = () => {
+    const starRef = useRef(null);
 
-    useGSAP(async () => {
-        const app = new Application();
-
-        await app.init({
-            backgroundAlpha: 0,
-            resizeTo: window,
-        });
-
-        canvasRef.current?.appendChild(app.canvas);
-
-        const graphics = new PIXI.Graphics();
-
-        graphics.beginPath();
-        graphics.star(0, 0, 6, 10);
-        graphics.fill();
-        graphics.pivot.set(50, 50);
-        graphics.position.set(100, 100);
-
-        app.stage.addChild(graphics);
-
-        gsap.timeline({
-            repeat: -1,
-            yoyo: true,
-        }).to(graphics, {
-            pixi: {
-                alpha: 0,
-            },
-            duration: 1,
-        });
-
-        return () => {
-            app.destroy({ removeView: true });
-        };
+    useGSAP(() => {
+        gsap.to(starRef.current, { opacity: 1, repeat: -1, duration: 1, ease: 'bonce' });
     });
 
-    return <div ref={canvasRef}></div>;
+    return <Sprite ref={starRef} image={'star.png'} width={100} height={100} visible={false} />;
+};
+
+const Canvas = () => {
+    return (
+        <Stage width={800} height={800} options={{ background: '#E9E9E9' }}>
+            <Star />
+        </Stage>
+    );
 };
 
 export default Canvas;
