46424de0069fb940d2450b07d1144ac7ebbb89c1
didoslavov <didkoslawow@gmail.com>
2024-04-14 23:53:27 +0300

    swap plain gsap animation with pixijs canvas animation due to performance issues

--- app/Components/ShootingStars/Canvas.tsx
+++ app/Components/ShootingStars/Canvas.tsx
@@ -1,31 +1,69 @@
 'use client';
 
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as PIXI from 'pixi.js';
 import gsap from 'gsap';
 import { PixiPlugin } from 'gsap/PixiPlugin';
-import { useGSAP } from '@gsap/react';
-import { Application } from 'pixi.js';
-import { Sprite, Stage } from '@pixi/react';
+import { Stage } from '@pixi/react';
+import StaticStar from './StaticStars';
+import FallingStar from './FallingStars';
 
 gsap.registerPlugin(PixiPlugin);
 PixiPlugin.registerPIXI(PIXI);
 
-const Star = () => {
-    const starRef = useRef(null);
-
-    useGSAP(() => {
-        gsap.to(starRef.current, { opacity: 1, repeat: -1, duration: 1, ease: 'bonce' });
-    });
-
-    return <Sprite ref={starRef} image={'star.png'} width={100} height={100} visible={false} />;
-};
-
 const Canvas = () => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [canvasDimensions, setCanvasDimensions] = useState({
+        width: 800,
+        height: 600,
+    });
+    const [stars, setStars] = useState<JSX.Element[]>([]);
+    const [stars1, setStars1] = useState<JSX.Element[]>([]);
+
+    const staticCount = 300;
+    const fallingCount = 30;
+
+    useEffect(() => {
+        const updateCanvasDimensions = () => {
+            const width = containerRef.current?.offsetWidth;
+            const height = containerRef.current?.offsetHeight;
+
+            if (width && height) {
+                setCanvasDimensions({ width, height });
+            }
+        };
+
+        updateCanvasDimensions();
+        window.addEventListener('resize', updateCanvasDimensions);
+
+        return () => {
+            window.removeEventListener('resize', updateCanvasDimensions);
+        };
+    }, []);
+
+    useEffect(() => {
+        const staticStars: JSX.Element[] = [];
+        const fallingStars: JSX.Element[] = [];
+
+        for (let i = 0; i < staticCount; i++) {
+            staticStars.push(<StaticStar key={`static-${i}`} />);
+        }
+
+        for (let i = 0; i < fallingCount; i++) {
+            fallingStars.push(<FallingStar key={`falling-${i}`} />);
+        }
+
+        setStars(() => [...staticStars]);
+        setStars1(() => [...fallingStars]);
+    }, []);
+
     return (
-        <Stage width={800} height={800} options={{ background: '#E9E9E9' }}>
-            <Star />
-        </Stage>
+        <div className="w-screen h-screen absolute top-0 right-0" ref={containerRef}>
+            <Stage width={canvasDimensions.width} height={canvasDimensions.height} options={{ backgroundAlpha: 0 }}>
+                {stars}
+                {stars1}
+            </Stage>
+        </div>
     );
 };
 
