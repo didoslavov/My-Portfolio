d378dd0d36a9411da0a8988befb4077c3c62489e
didoslavov <didkoslawow@gmail.com>
2024-04-15 03:05:10 +0300

    add theme provider

---
+++ app/providers.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+
+const Providers = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+};
+
+export default Providers;
