cbbfec2554adf4f91c895c87456658fb86b8ccf8
didoslavov <didkoslawow@gmail.com>
2024-04-16 00:02:17 +0300

    optimize canvas component

--- app/Components/ShootingStars/Canvas.tsx
+++ app/Components/ShootingStars/Canvas.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef, useState, useMemo, useEffect } from 'react';
 import * as PIXI from 'pixi.js';
 import gsap from 'gsap';
 import { PixiPlugin } from 'gsap/PixiPlugin';
@@ -19,23 +19,14 @@ const Canvas = () => {
         width: 800,
         height: 600,
     });
-    const [staticStars, setStaticStars] = useState<JSX.Element[]>([]);
-    const [fallingStars, setFallingStars] = useState<JSX.Element[]>([]);
 
-    const staticCount = 300;
-    const fallingCount = 20;
+    const smallStarsCount = 2000;
+    const bigStarsCount = 50;
+    const fallingStarsCount = 20;
 
     useEffect(() => {
-        const updateCanvasDimensions = () => {
-            const width = containerRef.current?.offsetWidth;
-            const height = containerRef.current?.offsetHeight;
-
-            if (width && height) {
-                setCanvasDimensions({ width, height });
-            }
-        };
-
         updateCanvasDimensions();
+
         window.addEventListener('resize', updateCanvasDimensions);
 
         return () => {
@@ -43,26 +34,44 @@ const Canvas = () => {
         };
     }, []);
 
-    useEffect(() => {
-        const staticStars: JSX.Element[] = [];
-        const fallingStars: JSX.Element[] = [];
-
-        for (let i = 0; i < staticCount; i++) {
-            staticStars.push(<StaticStar resolvedTheme={resolvedTheme} key={`static-${i}`} />);
+    const staticStarsSmall = useMemo(() => {
+        const stars = [];
+        for (let i = 0; i < smallStarsCount; i++) {
+            stars.push(<StaticStar starType="small" resolvedTheme={resolvedTheme} key={`staticSmall-${i}`} />);
         }
-
-        for (let i = 0; i < fallingCount; i++) {
-            fallingStars.push(<FallingStar resolvedTheme={resolvedTheme} key={`falling-${i}`} />);
-        }
-
-        setStaticStars(() => [...staticStars]);
-        setFallingStars(() => [...fallingStars]);
+        return stars;
     }, [resolvedTheme]);
+
+    const staticStarsBig = useMemo(() => {
+        const stars = [];
+        for (let i = 0; i < bigStarsCount; i++) {
+            stars.push(<StaticStar starType="big" resolvedTheme={resolvedTheme} key={`staticBig-${i}`} />);
+        }
+        return stars;
+    }, [resolvedTheme]);
+
+    const fallingStars = useMemo(() => {
+        const stars = [];
+        for (let i = 0; i < fallingStarsCount; i++) {
+            stars.push(<FallingStar resolvedTheme={resolvedTheme} key={`falling-${i}`} />);
+        }
+        return stars;
+    }, [resolvedTheme]);
+
+    const updateCanvasDimensions = () => {
+        const width = containerRef.current?.offsetWidth;
+        const height = containerRef.current?.offsetHeight;
+
+        if (width && height) {
+            setCanvasDimensions({ width, height });
+        }
+    };
 
     return (
         <div className="w-screen h-screen absolute top-0 right-0" ref={containerRef}>
             <Stage width={canvasDimensions.width} height={canvasDimensions.height} options={{ backgroundAlpha: 0 }}>
-                {staticStars}
+                {staticStarsSmall}
+                {staticStarsBig}
                 {fallingStars}
             </Stage>
         </div>
