67c43514d2c6e2516427704b023873ab34ce35ca
didoslavov <didkoslawow@gmail.com>
2024-05-04 02:26:00 +0300

    add logo to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,6 +7,10 @@ import MobileImage from './mobile/MobileImage';
 import Nav from './nav';
 import MobileNavMenu from './mobile/MobileNavMenu';
 import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import logoBgLight from '/public/logo-bg-light.png';
+import logoBgDark from '/public/logo-bg-dark.png';
+import Link from 'next/link';
 
 const Header = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -21,7 +25,12 @@ const Header = () => {
     };
 
     return (
-        <header className="py-6 px-10 flex justify-between items-center m-auto mb-8 relative z-50">
+        <header className="max-w-[1440px] flex justify-between items-center m-auto mb-8 relative z-50">
+            <div className="max-w-32">
+                <Link href="/">
+                    {resolvedTheme === 'dark' ? <Image src={logoBgDark} alt="Logo" /> : <Image src={logoBgLight} alt="Logo" />}
+                </Link>
+            </div>
             <ThemeButton isMobileMenuOpen={isMobileMenuOpen} toggleTheme={toggleTheme} />
             {!isMobileMenuOpen && <MobileImage isMobileMenuOpen={isMobileMenuOpen} />}
             {!isMobileMenuOpen && <MobileButton toggleMobileMenu={toggleMobile} isMobileMenuOpen={isMobileMenuOpen} />}
