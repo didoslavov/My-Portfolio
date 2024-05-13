ee27a03131e02f6860bb7a6b57c9692eae7e8d26
didoslavov <didkoslawow@gmail.com>
2024-05-13 14:54:01 +0300

    use api route

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,13 @@
 import { BentoGridProjcts } from '@/components/projects/projects';
+import { Project } from '@/types/projects';
 
-const Projects = () => {
+const Projects = async () => {
+    const response = await fetch(`${process.env.BASE_URL}/api/github/projects`);
+    const projects = await response.json();
+
     return (
         <section className="m-auto flex flex-wrap max-w-[1440px] gap-8 justify-between">
-            <BentoGridProjcts />
+            <BentoGridProjcts projects={projects.filter((p: Project) => p.stargazers_count > 2)} />
         </section>
     );
 };
