972070dfeeaffdceb083b086b3e6191c24ad1f39
didoslavov <didkoslawow@gmail.com>
2024-05-14 04:29:49 +0300

    fix tailwind config colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,29 +11,30 @@ const config: Config = {
     ],
     darkMode: 'class',
     theme: {
-        extend: {},
-        colors: {
-            'sheen-gold': {
-                DEFAULT: '#CAA45D',
-                300: '#dfc89e',
-                200: '#e5d2ae',
-                100: '#eadbbe',
-            },
-            'raisin-black': '#291F28',
-            'taupe-gray': '#958D98',
-            wenge: '#605862',
-            wine: {
-                DEFAULT: '#7F3343',
-                900: '#190a0d',
-                800: '#260f14',
-                700: '#33141b',
-            },
-            silver: {
-                DEFAULT: '#BBB4B3',
-                100: '#EAE9EA',
-                200: '#D4D2D4',
-                300: '#BFBCBF',
-                400: '#A9A5A9',
+        extend: {
+            colors: {
+                'sheen-gold': {
+                    DEFAULT: '#CAA45D',
+                    300: '#dfc89e',
+                    200: '#e5d2ae',
+                    100: '#eadbbe',
+                },
+                'raisin-black': '#291F28',
+                'taupe-gray': '#958D98',
+                wenge: '#605862',
+                wine: {
+                    DEFAULT: '#7F3343',
+                    900: '#190a0d',
+                    800: '#260f14',
+                    700: '#33141b',
+                },
+                silver: {
+                    DEFAULT: '#BBB4B3',
+                    100: '#EAE9EA',
+                    200: '#D4D2D4',
+                    300: '#BFBCBF',
+                    400: '#A9A5A9',
+                },
             },
         },
     },
