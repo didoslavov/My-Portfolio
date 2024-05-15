da9ca6a18949d948dd2cac419b8a73f384340235
didoslavov <didkoslawow@gmail.com>
2024-05-16 01:40:46 +0300

    add certificates assets

---
+++ src/constans/certificates.ts
@@ -0,0 +1,33 @@
+import diploma from "/public/certificates/diploma.png";
+import basics from "/public/certificates/basics.png";
+import fund from "/public/certificates/fund.png";
+import advanced from "/public/certificates/advanced.png";
+import applications from "/public/certificates/applications.png";
+import backend from "/public/certificates/backend.png";
+import react from "/public/certificates/react.png";
+import angular from "/public/certificates/angular.png";
+import mysql from "/public/certificates/mysql.png";
+import html from "/public/certificates/html.png";
+import htmlcss from "/public/certificates/htmlcss.png";
+import htmlsass from "/public/certificates/htmlsass.png";
+import ts from "/public/certificates/ts.png";
+import reactts from "/public/certificates/reactts.png";
+import zod from "/public/certificates/zod.png";
+
+export const certificates = [
+  { id: 1, src: diploma, alt: "Certificate image" },
+  { id: 2, src: basics, alt: "Certificate image" },
+  { id: 3, src: fund, alt: "Certificate image" },
+  { id: 4, src: advanced, alt: "Certificate image" },
+  { id: 5, src: applications, alt: "Certificate image" },
+  { id: 6, src: backend, alt: "Certificate image" },
+  { id: 7, src: react, alt: "Certificate image" },
+  { id: 8, src: angular, alt: "Certificate image" },
+  { id: 9, src: mysql, alt: "Certificate image" },
+  { id: 10, src: html, alt: "Certificate image" },
+  { id: 11, src: htmlcss, alt: "Certificate image" },
+  { id: 12, src: htmlsass, alt: "Certificate image" },
+  { id: 13, src: ts, alt: "Certificate image" },
+  { id: 14, src: reactts, alt: "Certificate image" },
+  { id: 15, src: zod, alt: "Certificate image" },
+];
