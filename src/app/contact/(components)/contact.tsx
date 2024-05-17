62389a524322d60d025066891e41f3211e281108
didoslavov <didkoslawow@gmail.com>
2024-05-17 04:17:17 +0300

    add form validations

--- src/app/contact/(components)/contact.tsx
+++ src/app/contact/(components)/contact.tsx
@@ -10,12 +10,20 @@ import {
 } from "@/utils/animations/animatePage";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export type FormData = {
-  name: string;
-  email: string;
-  message: string;
-};
+const schema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  message: z
+    .string()
+    .min(10, { message: "Message must be at least 10 characters long" }),
+});
+
+export type FormData = z.infer<typeof schema>;
 
 const Contact = () => {
   const router = useRouter();
@@ -24,7 +32,7 @@ const Contact = () => {
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema), mode: "onChange" });
   const contentRef = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
@@ -32,8 +40,8 @@ const Contact = () => {
     animateEnterPage(contentRef);
   });
 
-  function onSubmit(data: FormData) {
-    sendEmail(data);
+  function onSubmit(formData: z.infer<typeof schema>) {
+    sendEmail(formData);
     router.push("/contact?modal=true");
     reset();
   }
@@ -86,9 +94,14 @@ const Contact = () => {
           <input
             type="text"
             placeholder="John Doe"
-            className="w-full rounded-md border border-transparent bg-raisin-black bg-opacity-20 px-6 pb-3 pt-2 text-base text-raisin-black outline-none placeholder:text-lg placeholder:text-taupe-gray focus:border-wine focus:shadow-md dark:bg-silver dark:focus:border-sheen-gold"
+            className={`w-full rounded-md border border-transparent bg-raisin-black bg-opacity-20 px-6 pb-3 pt-2 text-base text-raisin-black outline-none placeholder:text-lg placeholder:text-taupe-gray focus:border-raisin-black focus:shadow-md dark:bg-silver dark:focus:border-sheen-gold ${errors.name && "border-wine"}`}
             {...register("name", { required: true })}
           />
+          {errors.name && (
+            <p className="font-concert text-sm text-wine">
+              {errors.name.message}
+            </p>
+          )}
         </div>
         <div className="mb-5">
           <label
@@ -100,9 +113,14 @@ const Contact = () => {
           <input
             type="email"
             placeholder="example@domain.com"
-            className="w-full rounded-md border border-transparent bg-raisin-black bg-opacity-20 px-6 pb-3 pt-2 text-lg text-raisin-black outline-none placeholder:text-lg placeholder:text-taupe-gray focus:border-wine focus:shadow-md dark:bg-silver dark:focus:border-sheen-gold"
+            className={`w-full rounded-md border border-transparent bg-raisin-black bg-opacity-20 px-6 pb-3 pt-2 text-lg text-raisin-black  outline-none placeholder:text-lg placeholder:text-taupe-gray focus:border-raisin-black focus:shadow-md dark:bg-silver dark:focus:border-sheen-gold ${errors.email && "border-wine"}`}
             {...register("email", { required: true })}
           />
+          {errors.email && (
+            <p className="font-concert text-sm text-wine">
+              {errors.email.message}
+            </p>
+          )}
         </div>
         <div className="mb-5">
           <label
@@ -114,9 +132,14 @@ const Contact = () => {
           <textarea
             rows={4}
             placeholder="Type your message"
-            className="w-full resize-none rounded-md border border-transparent bg-raisin-black bg-opacity-20 px-6 pb-3 pt-2 text-lg text-raisin-black outline-none placeholder:text-lg placeholder:text-taupe-gray focus:border-wine focus:shadow-md dark:bg-silver dark:focus:border-sheen-gold"
+            className={`w-full resize-none rounded-md border border-transparent bg-raisin-black bg-opacity-20 px-6 pb-3 pt-2 text-lg text-raisin-black outline-none placeholder:text-lg placeholder:text-taupe-gray focus:border-raisin-black focus:shadow-md dark:bg-silver dark:focus:border-sheen-gold ${errors.message && "border-wine"}`}
             {...register("message", { required: true })}
           ></textarea>
+          {errors.message && (
+            <p className="font-concert text-sm text-wine">
+              {errors.message.message}
+            </p>
+          )}
         </div>
         <div className="text-center">
           <button className="hover:shadow-form rounded-md bg-wine px-8 py-3 text-lg font-semibold text-silver outline-none hover:bg-wine-700 dark:bg-sheen-gold dark:text-raisin-black dark:hover:bg-sheen-gold-500 md:mt-10">
--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -11,15 +11,15 @@ function Modal() {
   return (
     <>
       {modal && (
-        <dialog className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center overflow-auto bg-black bg-opacity-50 backdrop-blur">
-          <div className="m-auto max-w-80 rounded-xl bg-raisin-black p-8 text-center text-silver dark:bg-silver dark:text-raisin-black md:min-w-96 md:max-w-full md:text-start">
-            <div className="flex flex-col items-center gap-6 text-base md:text-xl">
+        <dialog className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center overflow-auto bg-raisin-black bg-opacity-50 backdrop-blur">
+          <div className="m-auto max-w-80 rounded-xl bg-silver p-8 text-center text-raisin-black dark:bg-raisin-black dark:text-silver md:min-w-96 md:max-w-full md:text-start">
+            <div className="flex flex-col items-center gap-6 text-lg md:text-xl">
               <h3>Thanks for reaching out, I&apos;ll get back too you soon.</h3>
               <br />
               <Link href={pathname}>
                 <button
                   type="button"
-                  className="rounded-md bg-wine px-10 pb-2 pt-0 text-raisin-black hover:bg-wine-700 dark:bg-sheen-gold dark:hover:bg-sheen-gold-500 "
+                  className="rounded-md bg-wine px-10 py-2 text-silver hover:bg-wine-700 dark:bg-sheen-gold dark:text-raisin-black dark:hover:bg-sheen-gold-500 "
                 >
                   Close
                 </button>
