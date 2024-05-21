e39353f3b6f19b0806630a8174a315bb8abbc1b5
didoslavov <didkoslawow@gmail.com>
2024-05-21 23:58:07 +0300

    delete drizzle

--- drizzle/schema.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
-
-export const UsersTable = pgTable("guest-book", {
-  id: serial("id").primaryKey(),
-  user: text("user").notNull(),
-  message: text("message").notNull(),
-  createdAt: timestamp("createdAt").defaultNow().notNull(),
-});
