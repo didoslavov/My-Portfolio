4a5ff7f7394b7b98690fc81e1bacab0ce232fc26
didoslavov <didkoslawow@gmail.com>
2024-05-24 16:29:27 +0300

    make makeClient server action

--- src/app/ApolloWrapper.tsx
+++ src/app/ApolloWrapper.tsx
@@ -1,39 +1,12 @@
 "use client";
 
-import { ApolloLink, HttpLink } from "@apollo/client";
-import {
-  ApolloNextAppProvider,
-  NextSSRInMemoryCache,
-  NextSSRApolloClient,
-  SSRMultipartLink,
-} from "@apollo/experimental-nextjs-app-support/ssr";
+import { makeClient } from "@/lib/makeClient";
+import { ApolloNextAppProvider } from "@apollo/experimental-nextjs-app-support/ssr";
 import React, { PropsWithChildren } from "react";
-
-const uri = process.env.NEXT_PUBLIC_HASURA_API_URL + "/api/hasura";
-
-function makeClient() {
-  const httpLink = new HttpLink({
-    uri,
-    fetchOptions: { cache: "no-store" },
-  });
-
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache(),
-    link:
-      typeof window === "undefined"
-        ? ApolloLink.from([
-            new SSRMultipartLink({
-              stripDefer: true,
-            }),
-            httpLink,
-          ])
-        : httpLink,
-  });
-}
 
 export function ApolloWrapper({ children }: PropsWithChildren) {
   return (
-    <ApolloNextAppProvider makeClient={makeClient}>
+    <ApolloNextAppProvider makeClient={() => makeClient()}>
       {children}
     </ApolloNextAppProvider>
   );
---
+++ src/lib/makeClient.ts
@@ -0,0 +1,32 @@
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  NextSSRInMemoryCache,
+  NextSSRApolloClient,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+const uri = process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT || "";
+const hasuraAdminSecret = process.env.HASURA_ADMIN_SECRET || "";
+
+export function makeClient() {
+  const httpLink = new HttpLink({
+    uri,
+    fetchOptions: { cache: "no-store" },
+    headers: {
+      "x-hasura-admin-secret": hasuraAdminSecret,
+    },
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
