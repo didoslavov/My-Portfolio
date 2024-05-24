46b5dafc8e498d47a67229399704e2c811bc8325
didoslavov <didkoslawow@gmail.com>
2024-05-24 16:37:49 +0300

    fix env variable for production

--- src/app/ApolloWrapper.tsx
+++ src/app/ApolloWrapper.tsx
@@ -1,12 +1,52 @@
 "use client";
 
-import { makeClient } from "@/lib/makeClient";
-import { ApolloNextAppProvider } from "@apollo/experimental-nextjs-app-support/ssr";
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  NextSSRApolloClient,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
 import React, { PropsWithChildren } from "react";
 
-export function ApolloWrapper({ children }: PropsWithChildren) {
+// const uri = process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT || "";
+// const hasuraAdminSecret = process.env.HASURA_ADMIN_SECRET || "";
+
+function makeClient(uri: string, hasuraAdminSecret: string) {
+  const httpLink = new HttpLink({
+    uri,
+    fetchOptions: { cache: "no-store" },
+    headers: {
+      "x-hasura-admin-secret": hasuraAdminSecret,
+    },
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function ApolloWrapper({
+  children,
+  credentials,
+}: PropsWithChildren & {
+  credentials: { uri: string; hasuraAdminSecret: string };
+}) {
   return (
-    <ApolloNextAppProvider makeClient={() => makeClient()}>
+    <ApolloNextAppProvider
+      makeClient={() =>
+        makeClient(credentials.uri, credentials.hasuraAdminSecret)
+      }
+    >
       {children}
     </ApolloNextAppProvider>
   );
