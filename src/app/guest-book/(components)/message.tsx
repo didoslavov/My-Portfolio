5789b3d02255de4abdb7d9d09ce348dfb04f545a
didoslavov <didkoslawow@gmail.com>
2024-05-25 20:09:48 +0300

    add random avatar icon for messages

--- src/app/guest-book/(components)/message.tsx
+++ src/app/guest-book/(components)/message.tsx
@@ -1,26 +1,29 @@
+"use client";
+
+import { messageIcons } from "@/constans/message-icons";
 import { GuestBookEntry } from "@/graphql/queries";
 import formatDate from "@/utils/formatDate";
+import { useState } from "react";
 import { AiOutlineDelete, AiOutlineMessage } from "react-icons/ai";
 import { BiMessageSquareEdit } from "react-icons/bi";
-import { FaUserAstronaut } from "react-icons/fa";
 
-const Message = ({
-  user,
-  message,
-  created_at,
-}: Pick<GuestBookEntry, "user" | "message" | "created_at">) => {
+const Message = ({ user, message, created_at }: Omit<GuestBookEntry, "id">) => {
+  const [avatarIcon] = useState(
+    messageIcons[Math.floor(Math.random() * messageIcons.length)],
+  );
+
   return (
-    <article className="rounded-md border border-sheen-gold bg-raisin-black bg-opacity-40 px-6 py-4 text-silver shadow-md shadow-sheen-gold">
+    <article className="rounded-md border border-wine bg-raisin-black bg-opacity-20 px-6 py-4 text-raisin-black shadow-md shadow-wine dark:border-sheen-gold dark:bg-opacity-40 dark:text-silver dark:shadow-sheen-gold">
       <div className="flex justify-between">
-        <div className="mb-6 flex w-1/3 items-center justify-center gap-6 rounded-md p-2 text-2xl text-silver">
-          <div className="w-fit rounded-full border-2 border-sheen-gold-500 bg-silver bg-opacity-20 p-2 text-3xl">
-            <FaUserAstronaut />
+        <div className="mb-6 flex w-1/5 items-center justify-center gap-6 rounded-md p-2 text-2xl text-raisin-black dark:text-silver">
+          <div className="w-fit rounded-full border-2 border-wine bg-silver bg-opacity-20 p-2 text-3xl dark:border-sheen-gold-500">
+            {avatarIcon}
           </div>
           <h3 className="text-3xl font-bold">{user}</h3>
         </div>
         <time className="font-concert text-lg">{formatDate(created_at)}</time>
       </div>
-      <div className="flex items-center justify-between rounded-md border-b border-t border-silver px-2 py-4">
+      <div className="flex items-center justify-between rounded-md border-b border-t border-raisin-black px-2 py-4 dark:border-silver">
         <div className="flex items-center gap-2">
           <AiOutlineMessage className="text-3xl" />
           <p className="text-lg font-semibold tracking-[0.9px]">{message}</p>
---
+++ src/constans/message-icons.tsx
@@ -0,0 +1,41 @@
+import { GiWomanElfFace } from "react-icons/gi";
+import { GiWizardFace } from "react-icons/gi";
+import { GiWitchFace } from "react-icons/gi";
+import { GiTreeFace } from "react-icons/gi";
+import { GiSpiderFace } from "react-icons/gi";
+import { GiInvisibleFace } from "react-icons/gi";
+import { GiDwarfFace } from "react-icons/gi";
+import { GiCyborgFace } from "react-icons/gi";
+import { GiMagicTrident } from "react-icons/gi";
+import { GiMagicSwirl } from "react-icons/gi";
+import { GiMagicShield } from "react-icons/gi";
+import { GiMagicPotion } from "react-icons/gi";
+import { GiMagicPortal } from "react-icons/gi";
+import { GiMagicGate } from "react-icons/gi";
+import { GiMagicAxe } from "react-icons/gi";
+import { GiMagicHat } from "react-icons/gi";
+import { GiSeaCreature } from "react-icons/gi";
+import { GiFrankensteinCreature } from "react-icons/gi";
+import { FaUserAstronaut } from "react-icons/fa";
+
+export const messageIcons = [
+  <FaUserAstronaut key="icon-astronaut" />,
+  <GiFrankensteinCreature key="icon-frankenstein" />,
+  <GiSeaCreature key="icon-sea-creature" />,
+  <GiMagicHat key="icon-magic-hat" />,
+  <GiMagicAxe key="icon-magc-axe" />,
+  <GiMagicGate key="icon-magic-gate" />,
+  <GiMagicPortal key="icon-magic-portal" />,
+  <GiMagicPotion key="icon-magic-potion" />,
+  <GiMagicShield key="icon-magic-shield" />,
+  <GiMagicSwirl key="icon-magic-swirl" />,
+  <GiMagicTrident key="icon-magic-trident" />,
+  <GiCyborgFace key="icon-cyborg" />,
+  <GiDwarfFace key="icon-dwarf" />,
+  <GiInvisibleFace key="icon-invisible" />,
+  <GiSpiderFace key="icon-spider" />,
+  <GiTreeFace key="icon-tree" />,
+  <GiWitchFace key="icon-witch" />,
+  <GiWizardFace key="icon-wizzard" />,
+  <GiWomanElfFace key="icon-elf" />,
+];
