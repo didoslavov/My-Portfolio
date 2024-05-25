22c71bf7d9b68f16e18fb13111f26ab1faa3a861
didoslavov <didkoslawow@gmail.com>
2024-05-25 20:11:25 +0300

    add insert comment mutation

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -17,3 +17,16 @@ export const GET_GUESTBOOK_ENTRIES = gql`
     }
   }
 `;
+
+export const ADD_GUESTBOOK_ENTRY = gql`
+  mutation InsertMessage($message: String!, $user: String!) {
+    insert_guest_book(objects: { message: $message, user: $user }) {
+      returning {
+        id
+        created_at
+        message
+        user
+      }
+    }
+  }
+`;
